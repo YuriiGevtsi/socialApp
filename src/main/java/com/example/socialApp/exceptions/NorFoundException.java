9a92ee209bd7cb470abd76494288cb3eb41a7fe9
Yurii Gevtsi <y.gevtsi@gmail.com>
2019-07-13 18:47:44 +0200

    adding to git

---
+++ src/main/java/com/example/socialApp/exceptions/NorFoundException.java
@@ -0,0 +1,9 @@
+package com.example.socialApp.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class NorFoundException extends RuntimeException {
+
+}
